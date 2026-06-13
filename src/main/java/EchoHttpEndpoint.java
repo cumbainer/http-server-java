af34d5885e3d1f66f6d9a3eb53c367f7f9238859
Vladyslav Shtaiier <cumbainer@MacBook-Pro-von-Vladyslav.local>
2026-06-13 18:59:00 +0300

    codecrafters submit [skip ci]

--- src/main/java/EchoHttpEndpoint.java
+++ src/main/java/EchoHttpEndpoint.java
@@ -1,10 +1,29 @@
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+import java.util.Map;
+import java.util.Set;
+import java.util.zip.GZIPOutputStream;
+
 public class EchoHttpEndpoint implements HttpEndpoint {
+    private static final Set<String> SUPPORTED_COMPRESSIONS = Set.of("gzip");
+    private static final String COMPRESSION_HEADER = "Accept-Encoding";
+
     private final HttpResponseBuilder responseBuilder;
 
     @Override
     public HttpResponse processRequest(HttpRequest request) {
         String[] parts = request.url().split("/");
         String echoPart = parts[2];
+
+        boolean hasCompression = request.headers().containsKey(COMPRESSION_HEADER);
+        boolean isValidCompression = SUPPORTED_COMPRESSIONS.contains(request.headers().get(COMPRESSION_HEADER));
+        if (hasCompression && isValidCompression) {
+            echoPart = compress(echoPart);
+            return responseBuilder.build200(echoPart, Map.of("Content-Encoding", "gzip"));
+        }
+
         return responseBuilder.build200(echoPart);
     }
 
@@ -20,5 +39,21 @@ public class EchoHttpEndpoint implements HttpEndpoint {
 
     public EchoHttpEndpoint(HttpResponseBuilder responseBuilder) {
         this.responseBuilder = responseBuilder;
+    }
+
+    public static String compress(String str) {
+        if (str == null || str.isEmpty()) {
+            return str;
+        }
+
+        ByteArrayOutputStream obj = new ByteArrayOutputStream();
+        try (GZIPOutputStream gzip = new GZIPOutputStream(obj)) {
+            gzip.write(str.getBytes(StandardCharsets.UTF_8));
+        }
+        catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        return Base64.getEncoder().encodeToString(obj.toByteArray());
     }
 }
